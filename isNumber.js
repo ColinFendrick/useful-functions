3ae0a49b28edc16181eefd435dc2cffa356863b4
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-22 13:26:33 -0400

    Updated styling

--- isNumber.js
+++ isNumber.js
@@ -1,10 +1,12 @@
 function isNumeric (e) {
-					const event = e || window.event;
-					let key = event.keyCode || event.which;
-					key = String.fromCharCode(key);
-					const regex = /[0-9]|\./;
-					if (!regex.test(key)) { 
-						event.returnValue = false;
-						if (event.preventDefault) event.preventDefault
-					}
-				}
+	const event = e || window.event;
+	let key = event.keyCode || event.which;
+	key = String.fromCharCode(key);
+	const regex = /[0-9]|\./;
+	if (!regex.test(key)) { 
+		event.returnValue = false;
+		if (event.preventDefault) { 
+			event.preventDefault
+		}
+	}
+}
